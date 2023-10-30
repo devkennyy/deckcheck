e91c71f5229c8e893febe1cce5ae99e9b582b4a4
devkennyy <devkenny@outlook.com>
2023-10-30 17:03:44 +1000

    handle deck input via link

--- src/components/DeckInput.js
+++ src/components/DeckInput.js
@@ -1,16 +1,41 @@
-// Manages the deck input and link import functionality.
 import React, { useState } from 'react';
 
 const DeckInput = ({ onDeckInput }) => {
   const [deckLink, setDeckLink] = useState('');
+  const [deckComposition, setDeckComposition] = useState([]);
 
   const handleInputChange = (e) => {
     setDeckLink(e.target.value);
   };
 
   const handleAnalyzeClick = () => {
-    // Implement logic to process the deck link and call onDeckInput with the data
+    // Extract the query part of the link, which starts after "?deck="
+    const query = deckLink.substring(deckLink.indexOf('?deck=') + 6);
+    console.log('Extracted query:', query);
+  
+    // Extract card numbers from the query (assuming numbers are separated by periods)
+    const cardNumbers = query.split('.').map(Number);
+    console.log('Extracted card numbers:', cardNumbers);
+  
+    // Filter out any NaN values
+    const filteredCardNumbers = cardNumbers.filter((num) => !isNaN(num));
+    console.log('Filtered card numbers:', filteredCardNumbers);
+  
+    // Use your data mapping to get card data for each number
+    const cardData = filteredCardNumbers.map((number) => {
+      const paddedNumber = String(number).padStart(4, '0');
+      const cardImagePath = `/cards/Card_${paddedNumber}_bake.png`;
+      console.log('Card data for number', number, ':', cardImagePath);
+      return { number, image: cardImagePath };
+    });
+    console.log('Card data:', cardData);
+  
+    setDeckComposition(cardData);
+    onDeckInput(cardData); // Pass the deck composition to the parent component
   };
+  
+  
+  
 
   return (
     <div>
@@ -21,6 +46,7 @@ const DeckInput = ({ onDeckInput }) => {
         onChange={handleInputChange}
       />
       <button onClick={handleAnalyzeClick}>Analyze</button>
+      {/* Display deck composition based on the 'deckComposition' state */}
     </div>
   );
 };
