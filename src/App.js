ca7c4e1b61057aa77a390e0578749c1b55a52964
devkennyy <devkenny@outlook.com>
2023-10-31 16:01:18 +1000

    calculate totalcost

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DeckInput from './components/DeckInput';
+import DeckRating from './components/DeckRating'; // Import the DeckRating component
 import './App.css';
 import Card from './components/Card';
 
@@ -12,12 +13,16 @@ function App() {
     setLinkInputVisible(false);
   };
 
+  // Calculate the total cost based on deckComposition
+  const totalCost = deckComposition.reduce((total, card) => total + card.cost, 0);
+
   return (
     <div>
       {isLinkInputVisible ? (
         <DeckInput onDeckInput={handleDeckComposition} />
       ) : (
         <div>
+          <DeckRating totalCost={totalCost} />
           <h2>Deck Composition</h2>
           <div className="deck-images">
             {deckComposition.map((card, index) => (
--- src/components/DeckRating.js
+++ src/components/DeckRating.js
@@ -1,11 +1,30 @@
-// Highlights any issues or warnings related to the user's deck
 import React from 'react';
+import '../App.css';
 
-const DeckRating = ({ deckData }) => {
-  // Calculate and display the deck rating based on criteria (Attack, Defense, Synergy, Versatility)
+const DeckRating = ({ totalCost = 0, cardCount = 0 }) => {
+  // Calculate other ratings (attack, defense, synergy, versatility) here if needed
+
+  let costColorClass = '';
+
+  if (totalCost >= 49 && totalCost <= 50) {
+    costColorClass = 'green-cost';
+  } else if (totalCost >= 47 && totalCost <= 48) {
+    costColorClass = 'yellow-cost';
+  } else {
+    costColorClass = 'red-cost';
+  }
+
   return (
-    <div>
-      {/* Display deck rating and criteria explanations */}
+    <div className="deck-rating">
+      <h2>Deck Rating</h2>
+      <p>Total Cost: <span className={costColorClass}>{totalCost}</span></p>
+
+      <div className="rating-header">
+        <p>Attack: <span>x/10</span></p> 
+        <p>Defense: <span>x/10</span></p>
+        <p>Synergy: <span>x/10</span></p>
+        <p>Versatility: <span>x/10</span></p>
+      </div>
     </div>
   );
 };
