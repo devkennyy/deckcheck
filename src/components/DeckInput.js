e50c291eca0144e6164193a18d1faadb7835b0b4
devkennyy <devkenny@outlook.com>
2023-11-01 19:45:20 +1000

    handle personality card in deckInput

--- src/components/DeckInput.js
+++ src/components/DeckInput.js
@@ -8,6 +8,7 @@ const DeckInput = ({ onDeckInput }) => {
   const [deckComposition, setDeckComposition] = useState([]);
   const [isLinkInputVisible, setLinkInputVisible] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
+  const [personalityCard, setPersonalityCard] = useState(null);
 
   const handleInputChange = (e) => {
     setDeckLink(e.target.value);
@@ -16,17 +17,46 @@ const DeckInput = ({ onDeckInput }) => {
   const handleAnalyzeClick = () => {
     const query = deckLink.substring(deckLink.indexOf('?deck=') + 6);
     const cardNumbers = query.split('.').map(Number);
-    const filteredCardNumbers = cardNumbers.filter((num) => !isNaN(num));
-  
-    // Check if the card count is above 25
-    if (filteredCardNumbers.length > 25) {
-      setErrorMessage("Invalid deck: More than 25 cards detected");
+
+    // Ensure there is no more than one personality card
+    const personalityCardCount = cardNumbers.filter((num) => {
+      const cardInfo = allCards.find((card) => card.id === num);
+      return cardInfo && cardInfo.type === 'Personality';
+    }).length;
+
+    if (personalityCardCount > 1) {
+      setErrorMessage("Invalid deck: More than one personality card detected");
+      setDeckLink('');
+      return;
+    }
+
+    // Identify and set the personality card (if available)
+    const personalityCardInfo = allCards.find((card) => {
+      return card.type === 'Personality' && cardNumbers.includes(card.id);
+    });
+
+    if (personalityCardInfo) {
+      setPersonalityCard(personalityCardInfo);
+      console.log(`Personality Card Chosen: ${personalityCardInfo.name}`);
+    } else {
+      setPersonalityCard(null);
+    }
+
+    // Exclude the personality card
+    const nonPersonalityCardNumbers = cardNumbers.filter((num) => {
+      const cardInfo = allCards.find((card) => card.id === num);
+      return cardInfo && cardInfo.type !== 'Personality';
+    });
+
+    // Check if the card count is above 25 (excluding personality cards)
+    if (nonPersonalityCardNumbers.length > 25) {
+      setErrorMessage("Invalid deck: More than 25 cards detected (excluding personality cards)");
       setDeckLink('');
       return;
     }
 
     // Calculate the total cost of the deck
-    const totalCost = filteredCardNumbers.reduce((total, number) => {
+    const totalCost = nonPersonalityCardNumbers.reduce((total, number) => {
       const cardInfo = allCards.find((card) => card.id === number);
       if (cardInfo) {
         return total + cardInfo.cost;
@@ -41,20 +71,20 @@ const DeckInput = ({ onDeckInput }) => {
       return;
     }
 
-    // Check if the card count is less than 25
-    if (filteredCardNumbers.length < 25) {
-      setErrorMessage("Invalid deck: Less than 25 cards detected");
+    // Check if the card count is less than 25 (excluding personality cards)
+    if (nonPersonalityCardNumbers.length < 25) {
+      setErrorMessage("Invalid deck: Less than 25 cards detected (excluding personality cards)");
       setDeckLink('');
       return;
     }
 
-    const cardData = filteredCardNumbers.map((number) => {
+    const cardData = nonPersonalityCardNumbers.map((number) => {
       const cardInfo = allCards.find((card) => card.id === number);
       if (cardInfo) {
-        const { id, name, cost } = cardInfo;
+        const { id, name, cost, type } = cardInfo;
         const paddedNumber = String(id).padStart(4, '0');
         const cardImagePath = `/cards/Card_${paddedNumber}_bake.png`;
-        return { number, name, cost, image: cardImagePath };
+        return { number, name, cost, type, image: cardImagePath };
       }
       return null;
     }).filter((card) => card !== null);
@@ -87,7 +117,7 @@ const DeckInput = ({ onDeckInput }) => {
           ))}
         </div>
       )}
-
+        
       {errorMessage && (
         <div className="error-message">
           <p>{errorMessage}</p>
